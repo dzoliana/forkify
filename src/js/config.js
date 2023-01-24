1efc8ffef4326f967485e6a486825b2f5124ed7a
dzoliana <dzolicana@gmail.com>
2023-01-24 13:48:51 +0100

    Delete config

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const MODAL_CLOSE_SEC = 2.5;
-export const KEY = 'ea59532f-584b-4fe6-9513-d1a12d7a71ea';
